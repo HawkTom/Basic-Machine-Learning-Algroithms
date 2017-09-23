b231afc5b1f5bd7d3248ad7002077e19311b20f3
HawkTom <577254570@qq.com>
2017-09-24 00:00:21 +0800

    Update DecisionTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,1 +1,11 @@
-print("hello world")
+from sklearn.datasets import load_iris
+from sklearn import tree
+import os
+iris = load_iris()
+clf = tree.DecisionTreeClassifier()
+lf = clf.fit(iris.data, iris.target)
+
+with open(r"T:\iris.dot", 'w') as f:
+    f = tree.export_graphviz(clf, out_file=f)
+
+os.unlink('iris.dot')
